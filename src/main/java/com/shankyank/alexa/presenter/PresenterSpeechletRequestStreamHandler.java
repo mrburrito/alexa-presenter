d0ce161a872a1881b78f737bb53e4ae8faeba041
-- Gordon Shankman <-- gshankman@gmail.com>
2016-07-20 18:45:15 -0400

    Temporarily removed Application ID check for development. Added variations on names for presentations

--- src/main/java/com/shankyank/alexa/presenter/PresenterSpeechletRequestStreamHandler.java
+++ src/main/java/com/shankyank/alexa/presenter/PresenterSpeechletRequestStreamHandler.java
@@ -12,7 +12,7 @@ public final class PresenterSpeechletRequestStreamHandler extends SpeechletReque
     /** The Alexa application ID for the Presenter application. */
     private static final String PRESENTER_APPLICATION_ID = "";
     /** The set of application IDs that are valid for this service. */
-    private static final Set<String> SUPPORTED_APPLICATION_IDS = Collections.singleton(PRESENTER_APPLICATION_ID);
+    private static final Set<String> SUPPORTED_APPLICATION_IDS = Collections.emptySet();
 
     public PresenterSpeechletRequestStreamHandler() {
         super(new PresenterSpeechlet(), SUPPORTED_APPLICATION_IDS);
